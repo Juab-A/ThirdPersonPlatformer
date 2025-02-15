33a0859c98cceeb2c37d6c6ca8ee89dd24c2567b
Juan Ablan <ablanjuan1@gmail.com>
2025-02-15 13:53:11 -0800

    Implemented Points with Coins and GameManager

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Coin : MonoBehaviour
 {  
     [SerializeField] private float rotationSpeed = 50f;
+
+    public UnityEvent OnCollectCoin = new();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,6 +22,8 @@ public class Coin : MonoBehaviour
     void OnTriggerEnter (Collider collision)
     {
         if (collision.gameObject.CompareTag("Player")) {
+            OnCollectCoin?.Invoke();
+
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private float score = 0;
+
+    private Coin[] coins;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        scoreText.text = $"Score: {score}";
+
+        coins = FindObjectsByType<Coin>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+        foreach (Coin coin in coins) {
+            coin.OnCollectCoin.AddListener(IncrementScore);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void IncrementScore() {
+        score++;
+        scoreText.text = $"Score: {score}";
     }
 }
